219bf4038744a208101e529421c457d9e185ce02
Masu Lin <yosimasu@gmail.com>
2018-08-20 17:08:39 +0800

    initial commit

---
+++ app/rn-cli.config.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+var config = {
+    getProjectRoots() {
+        return [
+            // Keep your project directory.
+            path.resolve(__dirname),
+
+            // Include arc/root directory as a new root.
+            path.resolve(__dirname, "..")
+        ];
+    }
+}
+
+module.exports = config;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# REACT-NATIVE-MONO-STARTER
+
+## A React Native Starter based on `yarn workspaces`, and `lerna`.
