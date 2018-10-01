01a6a584281e0defe01412ab70af9a961012d119
Masu Lin <yosimasu@gmail.com>
2018-10-01 19:10:10 +0800

    chore: prerequisite

---
+++ scripts/prerequisite.sh
@@ -0,0 +1,12 @@
+source ~/.nvm/nvm.sh
+
+nvm install
+
+npm install -g yarn
+npm install -g lerna
+npm install -g react-native-cli
+npm install -g @storybook/cli
+npm install -g appcenter-cli
+npm install -g generator-rn-toolbox
+npm install -g shelljs
+npm install -g shx
