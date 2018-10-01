3cd8c900518916cd36c73a16d52ebb73768bc9e5
Masu Lin <yosimasu@gmail.com>
2018-10-01 13:17:50 +0800

    chore: Rename Module Prefix

--- modules/app/index.js
+++ modules/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from '@modules/components';
 
 export default class App extends React.Component {
   render() {
---
+++ modules/components/index.js
@@ -0,0 +1,3 @@
+export { default as StyleSheet } from './StyleSheet';
+export { default as Text } from './Text';
+export { default as View } from './View';
